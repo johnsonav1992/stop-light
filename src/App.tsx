66fbc07bdd44dd3b630474933a4840f7336d57e5
Alex Johnson <johnsonav1992@gmail.com>
2023-11-06 05:18:34 -0600

    okay signals are freaking cool

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-function App() {
+import { effect, signal } from "@preact/signals-react"
+import { Child } from "./Child";
 
+export const count = signal<number>(0)
+
+effect(() => {
+  if (count.value % 2 === 0) {
+    console.log('hello')
+  } else console.log('sup')
+})
+
+console.log(count.value)
+
+function App() {
   return (
-    <div>start</div>
-  )
+    <div>
+      <p>Count: {count}</p>
+      <Child />
+      <button onClick={() => count.value++}>click me</button>
+      <input type='number' onChange={e => count.value = Number(e.target.value)}/>
+    </div>
+  );
 }
 
 export default App
---
+++ src/Child.tsx
@@ -0,0 +1,6 @@
+import { count } from "./App"
+
+export const Child = () => {
+
+    return <div>{'child ' + count}</div>
+}
